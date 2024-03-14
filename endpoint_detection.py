ccfda8703a899cfb31ec1aba28fece6cfaa5478e
Lova Fenohasina RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2024-03-14 19:01:01 +0300

    feat: add response status code check function

--- endpoint_detection.py
+++ endpoint_detection.py
@@ -11,17 +11,35 @@ except Exception as e:
 
 pathList = []
 
+#check with http response status code if the path from request is accessible and not protected
+def isAccessiblePath(statusCode):
+    #get client error status code, resource protected or type of request not allowed by the server
+    if (statusCode in (401,403,404,405,407,410,414,418,421,451)):
+        return False
+    #get server error status code, need network authentication
+    if (statusCode in (511)):
+        return False
+    #get information response status code   -> (100-199)
+    #get successful response status code    -> (200-299)
+    #get redirection status code            -> (300-399)
+    #get client error status code, need to change to header or the body 
+    # -> (400-499) except for (401,403,404,405,407,410,414,418,421,451)
+    #get server error status code, implementation error or server connot handle unexpected client request or server not fonctional at the moment 
+    # -> (500-599) except for 511
+    return True
+
+
 def collectPathList(wordDict):
     for word in wordDict:
         word = word.rstrip()
         print(f'trying {word}')
         url = f'{base_url}/{word}'
         response = requests.get(url)
-        if response.status_code == 200 or response.status_code==500:
+        if isAccessiblePath(response.status_code):
             pathList.append(url)
 
 collectPathList(wordList)
 
-print("Path available:")
+print("Accessible paths:")
 for url in pathList:
     print(url)
